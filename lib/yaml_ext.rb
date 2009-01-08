1bf151e77b88e53b2c56453254929e6d1aabd66e
akimatter <akm2000@gmail.com>
2009-01-08 17:35:30 +0900

    改行入りのYAML と 改行入りの propertiesの相互変換が可能に。

--- lib/yaml_ext.rb
+++ lib/yaml_ext.rb
@@ -26,14 +26,20 @@ module YAML
       insert_text_with_indent(StringIO.new(text), options[:indent])
     end
     
-    def to_properties(filename)
+    def to_properties(filename, erb_enabled = false)
       result = ''
-      erb = ERB.new(IO.read(filename))
-      erb.filename = filename
-      text = erb.result
+      text = nil
+      if erb_enabled
+        erb = ERB.new(IO.read(filename))
+        erb.filename = filename
+        text = erb.result
+      else
+        text = IO.read(filename)
+      end
       prefixes = []
       StringIO.new(text).each_line do |line|
-        line = line.gsub(/\s*$/, '')
+        backslash_tail = /\\\s*$/ =~ line
+        line = line.gsub(/\s*\\{0,1}$/, '')
         case line
         when /^[\s\t]*$/
           # do nothing
@@ -47,47 +53,69 @@ module YAML
         else
           position = line.scan(/^\s*/).first.count(' ') / 2
           key, value = *line.split(':', 2).map{|s| s.strip}
-          value.gsub!(/^\"|\"$/, '')
-          result << (prefixes[0..(position - 1)] + [key]).join('.') << '=' << value
+          if value
+            value.gsub!(/^\"|\"$/, '')
+            result << (prefixes[0..(position - 1)] + [key]).join('.') << '=' << value
+          else
+            result << line.strip.gsub(/^\"|\"$/, '')
+          end
+          result << '\\' if backslash_tail
         end
         result << "\n"
       end
       result
     end
     
-    def from_properties(filename)
+    def from_properties(filename, erb_enabled = false)
       result = ''
-      erb = ERB.new(IO.read(filename))
-      erb.filename = filename
-      text = erb.result
+      text = nil
+      if erb_enabled
+        erb = ERB.new(IO.read(filename))
+        erb.filename = filename
+        text = erb.result
+      else
+        text = IO.read(filename)
+      end
       prefixes = []
+      in_multiline = false
       StringIO.new(text).each_line do |line|
-        line = line.gsub(/^\s|\s*$/, '')
-        case line
-        when /^[\s\t]*$/
-          # do nothing
-        when /^\s*\#.*\:\s*$/ # to_propertiesで入れたコメントは無視
-          next
-        when /^\s*\#/
-          result << line.strip
-        when /\=/
-          keys, value = line.split('=', 2)
-          paths = keys.split('.')
-          key = paths.pop
-          path_matching = true
-          paths.each_with_index do |path, index|
-            path_matching = (path == prefixes[index]) if path_matching
-            unless path_matching
-              result << (' ' * 2 * index) << path << ':' << "\n"
-            end
-          end
-          prefixes = paths
-          value = (/^\d+$|^\[.*\]$/ =~ value) ? value : '"%s"' % value
-          result << (' ' * 2 * paths.length) << key << ': ' << value
+        backslash_tail = /\\\s*$/ =~ line
+        line = line.gsub(/\s*\\{0,1}$/, '')
+        if in_multiline
+          result << (' ' * 2 * (prefixes.length + 1)) << line
+          result << '\\' if backslash_tail
+          result << '"' unless backslash_tail
         else
-          result << line
+          case line
+          when /^[\s\t]*$/
+            # do nothing
+          when /^\s*\#.*\:\s*$/ # to_propertiesで入れたコメントは無視
+            next
+          when /^\s*\#/
+            result << line.strip
+          when /\=/
+            keys, value = line.split('=', 2)
+            paths = keys.split('.')
+            key = paths.pop
+            path_matching = true
+            paths.each_with_index do |path, index|
+              path_matching = (path == prefixes[index]) if path_matching
+              unless path_matching
+                result << (' ' * 2 * index) << path << ':' << "\n"
+              end
+            end
+            prefixes = paths
+            unless /^\d+$|^\[.*\]$/ =~ value
+              value = (backslash_tail ?  '"%s' : '"%s"') % value
+            end
+            result << (' ' * 2 * paths.length) << key << ': ' << value
+            result << '\\' if backslash_tail
+          else
+            result << line
+          end
         end
         result << "\n"
+        in_multiline = backslash_tail
       end
       result
     end
